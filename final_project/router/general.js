87eae023471974f7fb00e78226505547f41c51ba
samanthachow0930-cmyk <samanthachow0930@gmail.com>
2025-09-01 09:01:44 -0400

    Updated general for task 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,12 +36,31 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
+// Get the book list available in the shop (Task 1)
 public_users.get('/',function (req, res) {
     res.send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN
+// Get the book list available in the shop (Task 10)
+public_users.get('/',function (req, res) {
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (books && Object.keys(books).length > 0) {
+                resolve(books);
+            } else {
+                reject(new Error("No books available"));
+            }
+        }, 100);
+    })
+    .then(booksData => {
+        res.status(200).json(booksData);
+    })
+    .catch(error => {
+        res.status(404).json({message: error.message});
+    });
+});
+
+// Get book details based on ISBN (Task 2)
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     if (isbn < 1 || isbn > 10){
@@ -51,8 +70,35 @@ public_users.get('/isbn/:isbn',function (req, res) {
     }
     res.send(books[isbn]);
 });
+
+// Get book details based on ISBN (Task 11)
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    
+    new Promise((resolve, reject) => {
+        // Async simulation
+        setImmediate(() => {
+            const num_isbn = parseInt(isbn);
+            
+            if (books[num_isbn]) {
+                resolve(books[num_isbn]);
+            } else {
+                reject(new Error("Book not found"));
+            }
+        });
+    })
+    .then(book => {
+        res.json(book);
+    })
+    .catch(error => {
+        res.status(404).json({ 
+            message: error.message,
+            available_isbns: Object.keys(books).join(', ')
+        });
+    });
+});
   
-// Get book details based on author
+// Get book details based on author (Task 3)
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     
@@ -70,7 +116,37 @@ public_users.get('/author/:author',function (req, res) {
     res.send(filtered_authors);
 });
 
-// Get all books based on title
+// Get book details based on author (Task 12)
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    new Promise((resolve, reject) => {
+        setImmediate(() => {
+            const filtered_books = Object.values(books).filter((book) => 
+                book.author && book.author.toLowerCase().trim() === author.toLowerCase().trim()
+            );
+            
+            if (filtered_books.length > 0) {
+                resolve(filtered_books);
+            } else {
+                reject(new Error("No books found for this author"));
+            }
+        });
+    })
+    .then(books => {
+        res.json({
+            author: author,
+            books: books
+        });
+    })
+    .catch(error => {
+        res.status(404).json({ 
+            message: error.message,
+            author: author
+        });
+    });
+});
+
+// Get all books based on title (Task 4)
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     
@@ -86,6 +162,37 @@ public_users.get('/title/:title',function (req, res) {
     }
     
     res.send(filtered_titles);
+});
+
+// Get book details based on title - simplified version
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+    new Promise((resolve, reject) => {
+        setImmediate(() => {
+            const filtered_books = Object.values(books).filter((book) => 
+                book.title && book.title.toLowerCase().trim() === title.toLowerCase().trim()
+            );
+            
+            if (filtered_books.length > 0) {
+                resolve(filtered_books);
+            } else {
+                reject(new Error("No books found with this title"));
+            }
+        });
+    })
+    .then(books => {
+        res.json({
+            title: title,
+            count: books.length,
+            books: books
+        });
+    })
+    .catch(error => {
+        res.status(404).json({ 
+            message: error.message,
+            title: title
+        });
+    });
 });
 
 //  Get book review
